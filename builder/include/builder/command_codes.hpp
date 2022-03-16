030150f917cc51a1a70bdcfded62ae9618fe8f3a
syoch <hacksyoch@gmail.com>
2022-03-16 21:18:33 +0900

    update: Added commandcodes definetion

---
+++ builder/include/builder/command_codes.hpp
@@ -0,0 +1,20 @@
+#ifndef BUILDER_COMMAND_CODES
+#define BUILDER_COMMAND_CODES
+
+#include "types.hpp"
+
+namespace builder {
+enum class Codes : u8 {
+  kWrite8 = 0x01,
+  kWrite16 = 0x02,
+  kWrite32 = 0x03,
+
+  kKernelWrite = 0x0B,
+  kKernelRead = 0x0C,
+
+  kReadMemory = 0x05,
+  kUploadMemory = 0x41,
+};
+}  // namespace builder
+
+#endif BUILDER_COMMAND_CODES
